4859c185da905a09d96c1bcddf7591debcb0b357
Pedro Paulo <pprego2@gmail.com>
2023-07-24 18:24:56 -0300

    Create aula33.py

---
+++ Aulas/aula33.py
@@ -0,0 +1,10 @@
+'''
+Introdução as funçoes (def) em python
+Funções são trechos de códigos usados para replicar
+determinadas ações ao longo do seu código
+Elas podem receber valores para parametros (argumentos)
+e retornar um valor especifico.
+Por padrão, funções python retornam Nome (nada)
+'''
+
+def 
